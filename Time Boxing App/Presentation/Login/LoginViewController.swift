c5b0c165a62a49992adfe515a56b3cfa38b2a47f
jperez96 <juliocesar.perezrabines@nttdata.com>
2022-06-20 15:40:49 -0500

    Adding project folder structure

---
+++ Time Boxing App/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Time Boxing App
+//
+//  Created by Julio Cesar Perez Rabines on 20/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
