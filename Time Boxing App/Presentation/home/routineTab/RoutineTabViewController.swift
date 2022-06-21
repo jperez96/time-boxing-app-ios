6f341e82a3329c3ece44fff32cb4414001a441ce
jperez96 <juliocesar.perezrabines@nttdata.com>
2022-06-20 22:15:13 -0500

    Created HomeStoryboard with their Tabs (Settings, Routines and Calenday files)

---
+++ Time Boxing App/Presentation/home/routineTab/RoutineTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RoutineTabViewController.swift
+//  Time Boxing App
+//
+//  Created by Julio Cesar Perez Rabines on 20/06/22.
+//
+
+import UIKit
+
+class RoutineTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
