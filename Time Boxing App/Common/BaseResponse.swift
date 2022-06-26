6736056acd804feecc3cdff92e356663e998583c
jperez96 <juliocesar.perezrabines@nttdata.com>
2022-06-26 00:16:23 -0500

    Created the Base Response class for responses from usecases and repositories

---
+++ Time Boxing App/Common/BaseResponse.swift
@@ -0,0 +1,25 @@
+//
+//  CustomResponse.swift
+//  Time Boxing App
+//
+//  Created by Julio Cesar Perez Rabines on 25/06/22.
+//
+
+import Foundation
+
+struct BaseResponse<T> {
+    var responseMessage: String = ""
+    var responseData: T
+    let responseCode: BaseResponseStatus
+    
+    static func success<T>(data: T, msg : String = "Ok." ) -> BaseResponse<T> {
+        return BaseResponse<T>(responseData: data, responseCode: .Success)
+    }
+    static func error<T>(data: T, msg : String = "Error." ) -> BaseResponse<T> {
+        return BaseResponse<T>(responseData: data, responseCode: .Error)
+    }
+}
+
+enum BaseResponseStatus {
+    case Success, Error
+}
